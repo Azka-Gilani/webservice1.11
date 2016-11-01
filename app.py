fc65ade4d6b492dca7bc6688c25072dfde2d05c9
Azka-Gilani <azka.gilani@gmail.com>
2016-11-01 14:36:16 +0500

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,6 @@ def processRequest(req):
     property_type=processPropertyType(req)
     unit_property=processUnit(req)
     area_property=processArea(req)
-    something=processUnitArea(unit_property, area_property)
     if "marla" in unit_property:
     	value=round(area_property*272.251,)
     minimum_value=processMinimum(req)
@@ -47,7 +46,7 @@ def processRequest(req):
         minimum_value,maximum_value=maximum_value,minimum_value
     else:
         minimum_value,maximum_value=minimum_value,maximum_value    
-    baseurl = "https://fazendanatureza.com/bot/botarz.php?city_name="+city_names+"&sector_name="+sector_names+"&minPrice="+minimum_value+"&maxPrice="+maximum_value+"&type="+property_type+"&LatestProperties="+ latest
+    baseurl = "https://fazendanatureza.com/bot/botarz.php?city_name="+city_names+"&sector_name="+sector_names+"&minPrice="+minimum_value+"&maxPrice="+maximum_value+"&type="+property_type+"&LatestProperties="+ latest+"&UnitArea="value
     result = urllib.urlopen(baseurl).read()
     data = json.loads(result)
     res = makeWebhookResult(data)
